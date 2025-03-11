e7964d8d3b6bd6f9490547f6399af91a6be4f40e
nelsonombuya <developer@incognitouser.anonaddy.me>
2025-03-11 15:28:19 +0300

    Update project version and add new TODO items for script management
    - Bump version to 2.1.311 in pyproject.toml and core version module
    - Add new TODO comments for script stopping functionality
    - Expand script management roadmap with granular script stopping options

--- scriptman/__init__.py
+++ scriptman/__init__.py
@@ -16,4 +16,10 @@ __all__: list[str] = ["powers", "config"]
 
 # TODO: Script aliases
 # TODO: Exit from scriptman while running things on multiple threads
-# TODO: Setting Literals on config leading to errors
+# TODO: Stop specific script from running
+# TODO: Stop all scripts
+# TODO: Stop all scripts of a particular user
+# TODO: Stop all scripts of a particular group
+# TODO: Stop all scripts of a particular name
+# TODO: Stop all scripts of a particular tag
+# TODO: Stop all scripts of a particular status

67b22f46f84be4bb7b1eb81f9f655dacb9fb9a2f
nelsonombuya <developer@incognitouser.anonaddy.me>
2025-06-14 17:00:02 +0300

    Bump project version to 2.9.499 and update commit count in version module

--- scriptman/core/_version.py
+++ scriptman/core/_version.py
@@ -19,7 +19,7 @@ class Version(BaseModel):
 
     major: int = Field(default=2, description="Major version number")
     minor: int = Field(default=9, description="Minor version number")
-    commit: int = Field(default=496, description="Commit count")
+    commit: int = Field(default=499, description="Commit count")
 
     @property
     def scriptman(self) -> str:
